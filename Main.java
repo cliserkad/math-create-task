b7a44074f7118383c52a59593be69c4cd325a364
Bryan Christopher Johnson <bryanchristopherjohnson@gmail.com>
2021-02-11 12:55:41 -0500

    Format java code. Add classpath and project to gitignore

--- Main.java
+++ Main.java
@@ -4,48 +4,48 @@ import java.util.ArrayList;
 
 public class Main {
 
-  public static void main(String[] args) {
-    final List<Double> list = new ArrayList<>();
-    final Scanner input = new Scanner(System.in);
-    print("Hello, I am the statistics machine");
-    print("Enter positive numbers to create a list to get the mean and standard deviation of the values.");
+	public static void main(String[] args) {
+		final List<Double> list = new ArrayList<>();
+		final Scanner input = new Scanner(System.in);
+		print("Hello, I am the statistics machine");
+		print("Enter positive numbers to create a list to get the mean and standard deviation of the values.");
 
-    double n = 0;
-    do {
-      String s = "";
-      try {
-        s = input.nextLine();
-        n = Double.parseDouble(s);
-        if(n >= 0) {
-          list.add(n);
-          print("Added " + list.get(list.size() - 1));
-        }
-      } catch (NumberFormatException nfe) {
-        print(s + " was not a positive number");
-      }
-    } while (n >= 0);
+		double n = 0;
+		do {
+			String s = "";
+			try {
+				s = input.nextLine();
+				n = Double.parseDouble(s);
+				if(n >= 0) {
+					list.add(n);
+					print("Added " + list.get(list.size() - 1));
+				}
+			} catch(NumberFormatException nfe) {
+				print(s + " was not a positive number");
+			}
+		} while(n >= 0);
 
-    double mean = sdOrMean(list, false, 0);
-    double stdDev = sdOrMean(list, true, mean);
+		double mean = sdOrMean(list, 0);
+		double stdDev = sdOrMean(list, mean);
 
-    print("The mean of your list is " + mean + ", and the standard deviation of your list is " + stdDev + "");
+		print("The mean of your list is " + mean + ", and the standard deviation of your list is " + stdDev + "");
 
-    input.close();
-  }
+		input.close();
+	}
 
-  /** convenience function because java can suck sometimes */
-  public static void print(Object o) {
-    System.out.println(o);
-  }
+	/** convenience function because java can suck sometimes */
+	public static void print(Object o) {
+		System.out.println(o);
+	}
 
-  public static int avg(Iterable<Integer> list) {
-    int sum = 0;
-    for(int num : list)
-      sum += num;
-    return sum;
-  }
+	public static int avg(Iterable<Integer> list) {
+		int sum = 0;
+		for(int num : list)
+			sum += num;
+		return sum;
+	}
 
-  public static long pow(final int number, int power) {
+	public static long pow(final int number, int power) {
 		if(power == 0 || number == 0) {
 			return 1;
 		}
@@ -57,7 +57,7 @@ public class Main {
 		return output;
 	}
 
-  public static double powD(final double number, double power) {
+	public static double powD(final double number, double power) {
 		if(power == 0 || number == 0) {
 			return 1;
 		}
@@ -69,23 +69,24 @@ public class Main {
 		return output;
 	}
 
-  public static double sdOrMean(Iterable<Double> list, boolean isStdDev, double mean) {
-    double sum = 0;
-    int amt = 0;
-    for(double num : list) {
-        if(isStdDev) {
-          sum += powD((num - mean), 2);
-        } else {
-          sum += num;
-        }
-        amt++;
-    }
+	public static double sdOrMean(Iterable<Double> list, double mean) {
 
-    if(isStdDev){
-      return Math.sqrt(sum / (amt - 1));
-    }
+		double sum = 0;
+		int amt = 0;
+		for(double num : list) {
+			if(mean != 0) {
+				sum += powD((num - mean), 2);
+			} else {
+				sum += num;
+			}
+			amt++;
+		}
 
-    return sum / amt;
-  }
+		if(mean != 0) {
+			return Math.sqrt(sum / (amt - 1));
+		}
+
+		return sum / amt;
+	}
 
 }
